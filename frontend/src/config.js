3dc79da8245f39375901844b9a2ff0eb16f2b6f9
Palak Agarwal <28palakagarwal@gmail.com>
2026-01-24 23:13:59 +0530

    Add backend updated path

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 export const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
-  "https://ai-study-buddy-react.onrender.com";
+    "https://ai-study-buddy-react-1.onrender.com";
 
 fetch(`${API_BASE_URL}/study/math`)
 fetch(`${API_BASE_URL}/study/summary`)
