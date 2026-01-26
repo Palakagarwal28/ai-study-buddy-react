a2b5f220a23ff67bd43a256b26c46254edfe6e27
Palakagarwal28 <28palakagarwal@gmail.com>
2026-01-27 00:31:09 +0530

    Update AppLayout.jsx

--- frontend/src/App/AppLayout.jsx
+++ frontend/src/App/AppLayout.jsx
@@ -4,8 +4,11 @@ import { Outlet } from "react-router-dom";
 
 export default function AppLayout() {
   return (
-    <div>
-      <Outlet />
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex-1 p-4 bg-slate-50">
+        <Outlet />
+      </div>
     </div>
   );
 }
