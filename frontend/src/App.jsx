ecaf365c6aaa7b0ab96e453871d782e81fd2012e
Palakagarwal28 <28palakagarwal@gmail.com>
2026-01-27 02:11:20 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home";
-import Login from "./Pages/Login";
-import Signup from "./Pages/Signup";
 
 import AppLayout from "./App/AppLayout";
 
