74dc064da72a2cdbc43e5d42f41ed4246151ae2e
Palakagarwal28 <28palakagarwal@gmail.com>
2026-01-26 15:02:08 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
@@ -11,6 +11,7 @@ import AppLayout from "./App/AppLayout";
 import { AuthProvider } from "./context/AuthContext";
 import { SavedProvider } from "./context/SavedContext";
 import ProtectedRoute from "./components/ProtectedRoute";
+
 import SummaryView from "./components/SummaryView";
 import QuizView from "./components/QuizView";
 import FlashcardView from "./components/FlashcardView";
@@ -25,17 +26,32 @@ export default function App() {
       <SavedProvider>
         <BrowserRouter>
           <Routes>
-          <Route path="/" element={<Home />} />
 
-            <Route path="/app" element={<AppLayout />}>
-            <Route path="summary" element={<SummaryView />} />
-            <Route path="quiz" element={<QuizView />} />
-            <Route path="flashcards" element={<FlashcardView />} />
-            <Route path="math" element={<MathSolverView />} />
-            <Route path="image" element={<ImageUpload />} />
-            <Route path="youtube" element={<YouTubeView />} />
-            <Route path="saved" element={<SavedSearches />} />
-          </Route>
+            {/* Default Redirect */}
+            <Route path="/" element={<Navigate to="/login" />} />
+
+            {/* Auth Pages */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+
+            {/* Protected App Routes */}
+            <Route
+              path="/app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route path="summary" element={<SummaryView />} />
+              <Route path="quiz" element={<QuizView />} />
+              <Route path="flashcards" element={<FlashcardView />} />
+              <Route path="math" element={<MathSolverView />} />
+              <Route path="image" element={<ImageUpload />} />
+              <Route path="youtube" element={<YouTubeView />} />
+              <Route path="saved" element={<SavedSearches />} />
+            </Route>
+
           </Routes>
         </BrowserRouter>
       </SavedProvider>
